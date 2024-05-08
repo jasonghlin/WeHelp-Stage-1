b67c72ef0abe5776124c39b6aa2819e9ba1f5419
GHL <devtest0204@gmail.com>
2024-05-08 16:17:52 +0800

    add middleware, function base program practice and fix minor bug

--- Week-6/Practice middleware and functional programming/app.py
+++ Week-6/Practice middleware and functional programming/app.py
@@ -8,10 +8,28 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
-
+from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
 
+class AuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request:Request, call_next):
+        if request.url.path.startswith("/static"):
+            return await call_next(request)
+        elif request.url.path == "/member":
+            if not request.session:
+                return RedirectResponse(url="/")
+        elif request.url.path == "/createMessage":
+            if not request.session:
+                return RedirectResponse(url="/")
+        elif request.url.path == "/deleteMessage":
+            if not request.session:
+                return RedirectResponse(url="/")
+        elif request.url.path not in ["/", "/member", "/error", "/signout", "/signup", "/signin", "/createMessage", '/deleteMessage']:
+            return RedirectResponse(url='/')
+        return await call_next(request)
+
+app.add_middleware(AuthMiddleware)
 app.add_middleware(SessionMiddleware, secret_key="some-random-string", session_cookie="session_id", max_age=300)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -38,16 +56,14 @@ async def route_index(request: Request):
   
     
 @app.get("/member", name="member")
-async def success(request: Request): 
-    if not request.session.get("username", False):
-        return RedirectResponse(url="/")
-    
+async def success(request: Request):    
     mycursor = mydb.cursor()
     message_query = ("SELECT message.id, name, message.content, message.time FROM member INNER JOIN message ON member.id = message.member_id ORDER BY message.time DESC" )
     mycursor.execute(message_query)
-    message_results = mycursor.fetchall()
+    mymessage_result = mycursor.fetchall()
+    # print(mymessage_result)
     mycursor.close()
-    return templates.TemplateResponse("loginstatus.html", {"request": request, "success_message": f"{request.session.get('name')}，歡迎登入系統", "messages": message_results, "user": request.session.get("username")})
+    return templates.TemplateResponse("loginstatus.html", {"request": request, "success_message": f"{request.session.get('name')}，歡迎登入系統", "messages": mymessage_result, "user": db_name})
 
 
 @app.get("/error")
@@ -63,15 +79,16 @@ async def signout(request: Request):
 
 @app.post("/signup", response_class=HTMLResponse, name="signup")
 async def create_user(request: Request, signup_name: str = Form(), signup_username: str = Form(), signup_password: str = Form()):
+    print(f"name: {signup_name} username: {signup_username}, password: {signup_password}")
     # check if there is any repeating username
     mycursor = mydb.cursor()
     query = ("SELECT username FROM member WHERE username = %s")
     val = (signup_username,)
     mycursor.execute(query, val)
-    myresult = mycursor.fetchone()
-    if myresult != []:
+    existing_user = mycursor.fetchall()
+    print(existing_user)
+    if existing_user != []:
         return RedirectResponse(url="/error?message=此帳號已被註冊", status_code=status.HTTP_303_SEE_OTHER)
-    
     # insert new user
     sql = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
     val = (signup_name, signup_username, signup_password)
@@ -87,12 +104,11 @@ async def login(request: Request, username: str = Form(), password: str = Form()
     query = ("SELECT id, name, username, password FROM member WHERE username = %s and password = %s")
     val=(username, password)
     mycursor.execute(query, val)
-    user = mycursor.fetchone()    
-    print(user)
+    user = mycursor.fetchone()
+    # print(myresult)
     if user == []:
         return RedirectResponse(url="/error?message=帳號、密碼輸入錯誤", status_code=status.HTTP_303_SEE_OTHER)
-    
-    db_id, db_name = user[0], user[1]
+    db_id, db_name, db_username, db_password = myresult[0]
     request.session.update({"id": db_id, "name": db_name, "username": username})
     mycursor.close()
     return RedirectResponse(url="/member", status_code=status.HTTP_303_SEE_OTHER)
@@ -104,6 +120,7 @@ async def create_message(request: Request, message: str = Form()):
     sql = "INSERT INTO message (member_id, content) VALUES (%s, %s)"
     val = (request.session.get("id"), message)
     mycursor.execute(sql, val)
+    
     mydb.commit()
     mycursor.close()
     return RedirectResponse(url="/member", status_code=status.HTTP_303_SEE_OTHER)
---
+++ Week-6/Practice middleware and functional programming/static/js/index.js
@@ -0,0 +1,66 @@
+let signupName = document.querySelector("#signup_name");
+let signupUsername = document.querySelector("#signup_username");
+let signupPassword = document.querySelector("#signup_password");
+let signupBtn = document.querySelector(".signup-btn");
+let signinUsername = document.querySelector("#username");
+let signinPassword = document.querySelector("#password");
+let signinBtn = document.querySelector(".signin-btn");
+let deleteBtns = document.querySelectorAll(".delete-btn");
+let messageInput = document.querySelector("#message");
+let messageSubmitBtn = document.querySelector(".message-submit-btn");
+
+if (signupBtn) {
+  signupBtn.addEventListener("click", function (e) {
+    if (
+      signupName.value === "" ||
+      signupUsername.value === "" ||
+      signupPassword.value === ""
+    ) {
+      e.preventDefault();
+      alert("請輸入完整的姓名、帳號、密碼");
+    }
+  });
+}
+
+if (signinBtn) {
+  signinBtn.addEventListener("click", function (e) {
+    if (signinUsername.value === "" || signinPassword.value === "") {
+      e.preventDefault();
+      alert("請輸入完整的帳號、密碼");
+    }
+  });
+}
+
+if (messageSubmitBtn) {
+  messageSubmitBtn.addEventListener("click", function (e) {
+    if (messageInput.value === "") {
+      e.preventDefault();
+      alert("請輸入留言內容");
+    }
+  });
+}
+
+if (deleteBtns) {
+  [...deleteBtns].forEach(function (button) {
+    button.addEventListener("click", async () => {
+      let messageId = button.closest(".message-with-btn").dataset.id;
+      if (confirm("您確定要刪除留言嗎？") == true) {
+        try {
+          let response = await fetch("/deleteMessage", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ messageId }),
+          });
+
+          if (response.ok) {
+            window.location.href = "http://127.0.0.1:8000/member";
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  });
+}
